d79d26292bfabb2bfe4d1b7e2e9d511fb20941e4
clbokea <clbo@kea.dk>
2021-10-25 15:27:09 +0200

    ses8

--- ses8/deck_of_card.py
+++ ses8/deck_of_card.py
@@ -4,3 +4,12 @@ class Deck:
 
     def __getitem__(self, key):
         return self.cards[key]
+    
+    def __setitem__(self, key, value):
+        self.cards[key] = value
+
+    def __delitem__(self, key):
+        del(self.cards[key])
+
+    def msg(self, x):
+        print(x)
