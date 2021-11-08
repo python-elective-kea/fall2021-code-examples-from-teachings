d1737b420ec21b45c267363c3257eedb3558337d
clbokea <clbo@kea.dk>
2021-11-08 12:27:18 +0100

    add.py added

---
+++ ses10/add.py
@@ -0,0 +1,30 @@
+# timer.py
+import time
+
+"""
+# decorator
+def timer(func):
+    def wrapper(*args, **kwargs):
+        start = time.time()
+        func(*args, **kwargs)
+        end = (time.time()) - start
+        print(f'Time elapsed: {end}')
+
+    return wrapper 
+"""
+
+# function
+#@timer
+def add(x):
+    l = []
+    for i in range(x):
+        l.append(i)
+    return l
+
+"""
+# Generator function
+@timer
+def gen_add(x):
+    for i in range(x):
+        yield i
+"""
