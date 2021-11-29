e1f54257c35eb3092a072aa797903954c889bb27
clbokea <clbo@kea.dk>
2021-11-29 10:30:49 +0100

    cp all files

---
+++ ses13/pdf_file_crack.py
@@ -0,0 +1,19 @@
+import pikepdf
+from tqdm import tqdm
+
+# load password list
+passwords = [ line.strip() for line in open("rockyou.txt", 'rb') ]
+
+# iterate over passwords
+for password in tqdm(passwords, "Decrypting PDF"):
+    try:
+        # open PDF file
+        with pikepdf.open("test.pdf", password=password) as pdf:
+            # Password decrypted successfully, break out of the loop
+            print("[+] Password found:", password)
+            break
+    except pikepdf._qpdf.PasswordError as e:
+        # wrong password, just continue in the loop
+        continue
+
+
